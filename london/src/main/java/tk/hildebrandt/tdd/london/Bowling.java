a1677b78a43f03330f64596f049053d23b08f7ab
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2022-05-03 16:07:27 +0200

    Setup for Bowling test

---
+++ london/src/main/java/tk/hildebrandt/tdd/london/Bowling.java
@@ -0,0 +1,13 @@
+package tk.hildebrandt.tdd.london;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bowling {
+   void roll(int pins) {
+   }
+
+   int score() {
+      return -1;
+   }
+}
