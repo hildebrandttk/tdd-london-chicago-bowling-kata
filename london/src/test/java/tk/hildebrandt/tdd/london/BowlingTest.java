6b47fedee10ed82ad06c57cf3942febadff96d9b
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2022-10-13 14:21:37 +0200

    Setup

--- london/src/test/java/tk/hildebrandt/tdd/london/BowlingTest.java
+++ london/src/test/java/tk/hildebrandt/tdd/london/BowlingTest.java
@@ -1,5 +1,10 @@
 package tk.hildebrandt.tdd.london;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BowlingTest {
+
+   private Bowling bowling = new Bowling();
 
 }
