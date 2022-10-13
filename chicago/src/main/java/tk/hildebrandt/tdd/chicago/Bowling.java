15d7be6dc06c7f3a89d613aa142a9a00ec7b0f41
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2022-10-13 09:57:50 +0200

    Chicago on unit interface

--- chicago/src/main/java/tk/hildebrandt/tdd/chicago/Bowling.java
+++ chicago/src/main/java/tk/hildebrandt/tdd/chicago/Bowling.java
@@ -1,10 +1,21 @@
 package tk.hildebrandt.tdd.chicago;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bowling {
+   private List<Integer> rolls = new ArrayList<>();
    void roll(int pins) {
+      rolls.add(pins);
    }
 
    int score() {
-      return -1;
+      int bonus = 0;
+      for (int i = 2; i < rolls.size(); i++) {
+         if(rolls.get(i-2) + rolls.get(i-1) == 10) {
+            bonus += rolls.get(i);
+         }
+      }
+      return rolls.stream().reduce(0, Integer::sum) + bonus;
    }
 }
--- chicago/src/test/java/tk/hildebrandt/tdd/chicago/BowlingTest.java
+++ chicago/src/test/java/tk/hildebrandt/tdd/chicago/BowlingTest.java
@@ -1,6 +1,8 @@
 package tk.hildebrandt.tdd.chicago;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingTest {
 
@@ -9,5 +11,33 @@ class BowlingTest {
    @BeforeEach
    void setUp() {
       game = new Bowling();
+   }
+
+   @Test
+   void scoreIs4AfterRolling4AsFirstRoll() {
+      game.roll(4);
+
+      assertThat(game.score()).isEqualTo(4);
+   }
+
+   @Test
+   void scoreIs9AfterRolling4And5AsFirstFrame() {
+      game.roll(4);
+      game.roll(5);
+
+      int score = game.score();
+
+      assertThat(score).isEqualTo(9);
+   }
+
+   @Test
+   void addBonusAfterRollingASpare() {
+      game.roll(4);
+      game.roll(6);
+      game.roll(7);
+
+      int score = game.score();
+
+      assertThat(score).isEqualTo(4 + 6 + 7 + 7);
    }
 }
